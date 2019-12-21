151e0b3b8d305babd1680f71121a7e3cfd5c4711
John Mulhern <john@shihtzu.io>
2019-12-21 07:52:52 -0700

    Tutorial - 5.5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root             'static_pages#home'
-  get 'home'    => 'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
+  root 'static_pages#home'
+  get '/home',    to:'static_pages#home'
+  get '/help',    to:'static_pages#help'
+  get '/about',   to:'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup',  to: 'users#new'
 end
