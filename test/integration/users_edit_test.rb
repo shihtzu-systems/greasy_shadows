07d17370878c163d84726935241a22f02ac4e15f
John Mulhern <john@shihtzu.io>
2020-02-17 15:16:23 -0700

    10.1.4 - added user edit

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user =  users(:john)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    patch user_path(@user), params: { user:
+                                          { name: '',
+                                            email: 'user@invalid',
+                                            password: 'blah',
+                                            password_confirmation: 'aaaaaa' } }
+    assert_template 'users/edit'
+  end
+
+  test 'successful edit' do
+    get edit_user_path(@user)
+    patch user_path(@user), params: { user:
+                                          { name: 'Joe Smith',
+                                            email: 'user@valid.com',
+                                            password: '',
+                                            password_confirmation: '' } }
+
+    assert_redirected_to @user
+    assert_not flash.empty?
+
+    @user.reload
+    assert_equal @user.name, 'Joe Smith'
+    assert_equal @user.email, 'user@valid.com'
+  end
+
+end
