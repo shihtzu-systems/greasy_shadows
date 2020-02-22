caf45b13f81add274e3047def11ea28ce0c000f5
John Mulhern <john@shihtzu.io>
2020-02-22 13:11:46 -0700

    10.4.2 the destroy action
    Signed-off-by: John Mulhern <john@shihtzu.io>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -41,11 +42,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = 'It gone!'
+    redirect_to users_path
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email,
-                                 :password, :password_confirmation)
+    params.require(:user).permit(:name,
+                                 :email,
+                                 :password,
+                                 :password_confirmation)
   end
 
   def logged_in_user
@@ -59,5 +68,9 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user&.admin?
   end
 end
