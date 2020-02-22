0351e6594713bcc5deca050fb5cbe3873a80ce4c
John Mulhern <john@shihtzu.io>
2020-02-22 13:36:50 -0700

    10.4.3 user destroy tests
    Signed-off-by: John Mulhern <john@shihtzu.io>

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -40,9 +40,24 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test 'should redirect update when logged in as wrong user' do
     log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
+    patch user_path(@user), params: {user: {name: @user.name,
+                                            email: @user.email}}
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when logged in as non-admin' do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
     assert_redirected_to root_url
   end
 end
